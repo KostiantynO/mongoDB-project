ff83ec9753b663c03cb80b84d6dc143d753a77f8
Kostiantyn Ochenash <Ochenashk@gmail.com>
2022-07-01 23:31:55 +0300

    adds basic template to request all products after click in link

--- server/app.js
+++ server/app.js
@@ -18,18 +18,23 @@ app.use(express.json());
 app.use(logger('dev'));
 
 app.get('/', (req, res) => {
-  res.send(
-    `<!DOCTYPE html>
-      <html>
-        <head>
-          <title>OnlineShop<title>
-        </head>
-        <body>
-          <a href="https://ko-mongodb-project.herokuapp.com/api/v1/products" rel="noopener noreferrer">Show products</a>
-        <body>
-      </html>
-    `,
-  );
+  res.send(`<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>OnlineShop</title>
+  </head>
+  <body>
+    <a
+      href="https://ko-mongodb-project.herokuapp.com/api/v1/products"
+      rel="noopener noreferrer nofollow"
+      target="_blank"
+      >Get all products</a
+    >
+  </body>
+</html>`);
 });
 
 app.use(products, productsRouter);
@@ -41,7 +46,8 @@ const main = async () => {
 
     app.listen(PORT);
 
-    console.log(`Endpoint: http://localhost:${PORT}${products}`);
+    // console.log(`Endpoint: http://localhost:${PORT}${products}`);
+    console.log(`Endpoint: http://localhost:${PORT}`);
   } catch (error) {
     console.log(error.message);
     process.exitCode = 1;
